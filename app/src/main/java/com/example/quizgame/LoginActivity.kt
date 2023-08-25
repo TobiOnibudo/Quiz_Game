a2eb4fb6cbab8fa66229ab578b3fa7997cfa9f60
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-25 11:34:07 -0300

    Implemented sign in persistence and sign out feature

--- app/src/main/java/com/example/quizgame/LoginActivity.kt
+++ app/src/main/java/com/example/quizgame/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.example.quizgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.quizgame.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginBinding: ActivityLoginBinding
+
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +21,11 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         loginBinding.buttonSignin.setOnClickListener {
+            val userEmail  = loginBinding.editTextLoginEmail.text.toString()
+            val userPassword  = loginBinding.editTextLoginPassword.text.toString()
 
+            loginBinding.progressBarLogin.visibility = View.VISIBLE
+            signInUser(userEmail,userPassword)
         }
 
         loginBinding.buttonGoogleSignin.setOnClickListener {
@@ -32,5 +41,39 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
+    }
+
+    private fun signInUser(userEmail: String, userPassword: String) {
+
+        auth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener {task->
+            if(task.isSuccessful)
+            {
+                loginBinding.progressBarLogin.visibility = View.INVISIBLE
+                Toast.makeText(applicationContext,"Welcome to Quiz Game",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else
+            {
+                Toast.makeText(applicationContext,task.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val user = auth.currentUser
+
+        if (user != null)
+        {
+            loginBinding.progressBarLogin.visibility = View.INVISIBLE
+            Toast.makeText(applicationContext,"Welcome to Quiz Game",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.quizgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizgame.databinding.ActivityMainBinding
+import com.example.quizgame.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+
+        setContentView(view)
+
+        mainBinding.buttonSignOut.setOnClickListener {
+            //sign user out and move back to log in page
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
