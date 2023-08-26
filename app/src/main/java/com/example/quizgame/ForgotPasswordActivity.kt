7f488f7b49df06d888ca918f031b832f45ec7e16
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-26 08:53:01 -0300

    Implemented Forgot password feature

--- app/src/main/java/com/example/quizgame/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/quizgame/ForgotPasswordActivity.kt
@@ -2,10 +2,36 @@ package com.example.quizgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.quizgame.databinding.ActivityForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private lateinit var forgotBinding : ActivityForgotPasswordBinding
+
+   private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        forgotBinding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        val view = forgotBinding.root
+        setContentView(view)
+
+
+        forgotBinding.buttonReset.setOnClickListener {
+            val userEmail = forgotBinding.editTextForgotEmail.text.toString()
+
+            auth.sendPasswordResetEmail(userEmail).addOnCompleteListener {task->
+                if(task.isSuccessful)
+                {
+                    Toast.makeText(applicationContext,"We sent a password reset mail to email address",Toast.LENGTH_SHORT).show()
+                    finish()
+                }else
+                {
+                    Toast.makeText(applicationContext,task.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
