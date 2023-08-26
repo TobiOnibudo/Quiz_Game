408cccfa6e655c63662794dc69655e34855adb7c
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-26 11:38:33 -0300

    Connected to database

--- app/src/main/java/com/example/quizgame/QuizActivity.kt
+++ app/src/main/java/com/example/quizgame/QuizActivity.kt
@@ -2,10 +2,53 @@ package com.example.quizgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizgame.databinding.ActivityQuizBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class QuizActivity : AppCompatActivity() {
+
+    private lateinit var quizBinding: ActivityQuizBinding
+
+    private val database = FirebaseDatabase.getInstance()
+    private val databaseReference = database.reference.child("questions")
+
+    private var question = ""
+    private var answerA = ""
+    private var answerB = ""
+    private var answerC = ""
+    private var answerD = ""
+    private var correctAnswer = ""
+
+    private var questionCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        quizBinding = ActivityQuizBinding.inflate(layoutInflater)
+        val view = quizBinding.root
+        setContentView(view)
+
+        quizBinding.buttonNext.setOnClickListener {
+
+        }
+
+        quizBinding.buttonFinish.setOnClickListener {
+
+        }
+
+        quizBinding.textViewA.setOnClickListener {
+
+        }
+
+        quizBinding.textViewB.setOnClickListener {
+
+        }
+        quizBinding.textViewC.setOnClickListener {
+
+        }
+
+        quizBinding.textViewD.setOnClickListener {
+
+        }
+
     }
 }
