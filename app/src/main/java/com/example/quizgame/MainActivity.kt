c9d3e895e16095212151159393a68562508291f9
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-08-26 10:11:00 -0300

    Implemented start game button

--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.quizgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.quizgame.databinding.ActivityMainBinding
 import com.example.quizgame.databinding.ActivitySignupBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -18,9 +21,28 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        mainBinding.buttonStartQuiz.setOnClickListener {
+            val intent  = Intent(this@MainActivity,QuizActivity::class.java)
+            startActivity(intent)
+        }
+
         mainBinding.buttonSignOut.setOnClickListener {
             //sign user out and move back to log in page
+
+            //email and password log out
             FirebaseAuth.getInstance().signOut()
+
+            //google account log out
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build()
+
+            val googleSignInClient = GoogleSignIn.getClient(this,gso)
+            googleSignInClient.signOut().addOnCompleteListener {task->
+                if (task.isSuccessful)
+                {
+                    Toast.makeText(applicationContext,"Sign out is successful",Toast.LENGTH_LONG).show()
+                }
+            }
 
             val intent = Intent(this@MainActivity,LoginActivity::class.java)
             startActivity(intent)
